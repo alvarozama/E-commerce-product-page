3f6a0ed284afa0513cfa5427789ac537adf2f9e0
Alvaro Zacarias <alvaro.zacmat@gmail.com>
2024-06-28 19:06:52 -0600

    Photo carousel added

--- index.js
+++ index.js
@@ -67,3 +67,27 @@ function toggleMobMenu() {
 
 toggleMobMenu();
 
+// Photo carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("my-slides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
